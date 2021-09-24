c1b228da39fd13adc63d4b822866913c80275bcb
Arpan Abhishek <arpanforbusiness@gmail.com>
2021-09-24 17:53:45 +0530

    add setup config

--- setup.py
+++ setup.py
@@ -1,14 +1,26 @@
 from setuptools import setup
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name='protectsql',
     version='0.0.1',
+    author="Arpan Abhishek",
+    author_email="arpanforbusiness@gmail.com",
+    description="A small package to track SQL injection in flask app",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/arpancodes/protectsql",
+    project_urls={
+        "Bug Tracker": "https://github.com/arpancodes/protectsql/issues",
+    },
     py_modules=['protectsql'],
     install_requires=[
         'Click',
-				'Flask',
-				'pyre-check',
+        'Flask',
+        'pyre-check',
     ],
+    package_dir={"": "."},
     entry_points={
         'console_scripts': [
             'protectsql = protectsql.commands:protectsql',
